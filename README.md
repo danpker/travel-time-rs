a3efc10ef07e8ff22384c9d780a95a7264ab6ba7
Daniel Parker <danpker@gmail.com>
2019-01-11 13:43:59 +0000

    Real basic implmentation with reqwest

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # travel-time-rs
+BitBar plugin to give travel time from Google Maps
 Travel Time, but in Rust
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+use std::env;
+
+fn main() -> Result<(), reqwest::Error> {
+    let mut args = env::args();
+    let _ = args.next();
+
+    let start = args.next().unwrap();
+    let end = args.next().unwrap();
+    let api_key = args.next().unwrap();
+
+    let url = format!("https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&mode=driving&departure_time=now&key={}", start, end, api_key);
+    let echo_json: serde_json::Value = reqwest::Client::new()
+        .get(&url)
+        .send()?
+        .json()?;
+
+    println!("{:#?}", echo_json);
+    Ok(())
 }
